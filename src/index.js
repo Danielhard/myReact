d254a972bd3d52298984e159970029dd713ac975
Danielhard <335805097@qq.com>
2020-07-21 22:13:28 +0800

    day 02

--- src/index.js
+++ src/index.js
@@ -2,25 +2,76 @@ import {
     Component,
     myReact
 } from '../myReact'
+window.Square = {}
+class Square extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null
+        }
+    }
+    render() {
+        window.Square[this.props['value']] = this
+        return (
+            <button className="square" onClick={() => this.setState({ value: 'x' })}>
+                {this.state.value ? this.state.value : ""}
+            </button>
+        )
+    }
 
+
+}
+class Board extends Component {
+    renderSquare(i) {
+        return (
+            <Square
+                value={i}
+            />
+        );
+    }
+    render() {
+        return (
+            <div>
+                <MyComponent />
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
 class MyComponent extends Component {
     render() {
-       return (
-           <div>
-               <span>
-                   hello
+        return (
+            <div>
+                <span>
+                    Tic
                </span>
-               <span>
-                   world
+                <span>
+                    Tac
                </span>
-           </div>
+                <span>
+                    Toe
+               </span>
+            </div>
 
-       )
+        )
     }
 }
 
-let a = <MyComponent name="a" id="ida"></MyComponent>
-
+let a = <Board />
 myReact.render(
     a,
     document.getElementById("app")
